3affa2dd311ef2232a83e64e23496af1ce1235e4
lumiang <costa.luizmiguel.ang@gmail.com>
2022-06-05 19:13:04 -0300

    Jogo pronto só falta a decoração.

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 const darth_vader = document.querySelector('.darth-vader');
 const x_wing = document.querySelector('.x-wing');
 
+var placar_recorde = 0
+var placar_atual = 0
+
 const jump = () => {
  darth_vader.classList.add('jump');
 
@@ -18,16 +21,28 @@ const loop = setInterval(() => {
 
    if (x_wingPosition <= 125 && x_wingPosition > 0 && darth_vaderPosition < 50) {
 
-     x_wing.style.animation = 'none';
-     x_wing.style.left = `${x_wingPosition}px`;
+      x_wing.style.display = 'none';
 
-     darth_vader.style.animation = 'none';
-     darth_vader.style.bottom = `${darth_vaderPosition}px`;
+      darth_vader.style.bottom = `${darth_vaderPosition}px`;
+      darth_vader.style.left = '75px';
 
-     darth_vader.src = './imagens/morte-darth-vader.gif';
+      darth_vader.src = './imagens/morte-darth-vader.gif';
 
+      x_wing.style.animation = 'none';
+      x_wing.style.left = `${x_wingPosition}px`;
+      
+      darth_vader.style.animation = 'none';
+      darth_vader.style.bottom = darth_vaderPosition -2 +"px";
+      
+      darth_vader.src = './imagens/morte-darth-vader.gif';
+
+      const morte_darth_vader = setInterval(() => {
+         darth_vader.style.display = 'none';
+      }, 1000);
+     
+      clearInterval(loop);
    }
 
-}, 10);
+},10);
 
 document.addEventListener('keydown' , jump);
